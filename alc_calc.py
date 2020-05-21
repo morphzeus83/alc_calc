f168d291dac1e39662add5b1a040cf026df4f303
Arthur <morphzeus83@gmail.com>
2020-05-21 19:16:20 +0200

    Added ifelse based on 0.3 pro mille

--- alc_calc.py
+++ alc_calc.py
@@ -1,10 +1,10 @@
 Groesse = int(input("Wie groß bist du in cm? "))
 Gewicht = int(input("Wie viel kg wiegst du? "))
-Geschlechtsabfrage = input("Bitte gebe dein Geschlecht ein (w/m): ")
 wr = 0.31223 - 0.006446 * Gewicht + 0.004466 * Groesse
 mr = 0.31608 - 0.004821 * Gewicht + 0.004432 * Groesse
 Geschlecht = {"w" : ["weiblich", wr], "m" : ["männlich", mr]}
 while True:
+    Geschlechtsabfrage = input("Bitte gebe dein Geschlecht ein (w/m): ")
     if Geschlechtsabfrage in Geschlecht:
         break
     print("Falsche Eingabe! Bitte nur w oder m eingeben!")
@@ -17,8 +17,8 @@ Promille_theo = Alk / (Gewicht * Geschlecht[Geschlechtsabfrage][1])
 Promille_clean = Promille_theo - Promille_theo * 0.15
 Promille = Promille_clean - Trink_zeit * 0.15
 print("Hallo! Du bist", Geschlecht[Geschlechtsabfrage][0], ", bist", Groesse, "cm groß und wiegst", Gewicht, "kg")
-while True:
-    if Promille > 0.3:
-        break
+if Promille >= 0.3:
     print("Du solltest nicht mehr fahren!!!")
+else:
+    print("Du darfst noch fahren, solltest aber nichts mehr trinken!")
 print("Du hast noch", Promille, "Promille im Blut")
